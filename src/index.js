d5348362f0049986fa1e8256bcd9bc09d7acacab
Matthew <marberrym@gmail.com>
2019-10-13 23:43:31 -0400

    added a navbar and redux/material and other dependancies.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Router from './Router';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
+import indigo from '@material-ui/core/colors/indigo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let theme = createMuiTheme({
+  palette: {
+    primary: grey,
+    secondary: indigo,
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+let Dom = () => (
+  <MuiThemeProvider theme={theme}>
+    <Router>
+      <App />
+    </Router>
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(<Dom />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
